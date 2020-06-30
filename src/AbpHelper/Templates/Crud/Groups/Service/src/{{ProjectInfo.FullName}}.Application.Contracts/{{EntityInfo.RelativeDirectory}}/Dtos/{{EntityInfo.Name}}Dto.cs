afbdfc23773bf43dde1df6cd900e8fb0fb4bb519
Enver Francisco <enver.francisco@gmail.com>
2020-06-30 01:04:34 -0400

    one to one relation added
    added at dtos, viewmodels for use large addres of classes for relation of attributes objects

--- src/AbpHelper/Templates/Crud/Groups/Service/src/{{ProjectInfo.FullName}}.Application.Contracts/{{EntityInfo.RelativeDirectory}}/Dtos/{{EntityInfo.Name}}Dto.cs
+++ src/AbpHelper/Templates/Crud/Groups/Service/src/{{ProjectInfo.FullName}}.Application.Contracts/{{EntityInfo.RelativeDirectory}}/Dtos/{{EntityInfo.Name}}Dto.cs
@@ -6,13 +6,19 @@ namespace {{ EntityInfo.Namespace }}.Dtos
     [Serializable]
     public class {{ EntityInfo.Name }}Dto : {{ EntityInfo.BaseType | string.replace "AggregateRoot" "Entity"}}Dto{{ if EntityInfo.PrimaryKey }}<{{ EntityInfo.PrimaryKey}}>{{ end }}
     {
-        {{~ for prop in EntityInfo.Properties ~}}
+        {{~ typePrimitive = ["int","object","short","char","float","double","bool","string","sbyte","decimal","uint","long","ulong","ushort","dynamic","Boolean","Byte","Int16","UInt16","Int32","UInt32","Int64","UInt64","Single", "DateTime"] ~}}            
+        {{~ for prop in EntityInfo.Properties ~}}        
         {{~ if prop | abp.is_ignore_property; continue; end ~}}
         {{~ if string.ends_with prop.Type ">" ~}}
-        public {{ prop.Type | string.replace ">" "Dto>"}} {{ prop.Name }}Dto { get; set; }
+        public {{ prop.Type | string.replace ">" "Dto>"}} {{ prop.Name }}Dto { get; set; }        
         {{~ else ~}}
-        public {{ prop.Type}} {{ prop.Name }} { get; set; }
-        {{~end ~}}
+        {{~ validateOfPrimite = typePrimitive | array.contains prop.Type ~}}
+        {{~ if !validateOfPrimite ~}}        
+        public {{ prop.Type }}Dto {{ prop.Name | string.downcase }}Dto { get; set; }
+        {{~ else ~}}
+        public {{ prop.Type }} {{ prop.Name }} { get; set; }
+        {{~ end ~}}
+        {{~ end ~}}
         {{~ if !for.last ~}}
 
         {{~ end ~}}
--- src/AbpHelper/Templates/Crud/Groups/UiRazor/src/{{ProjectInfo.FullName}}.Web/Pages/{{Bag.PagesFolder}}/{{EntityInfo.RelativeDirectory}}/{{EntityInfo.Name}}/ViewModels/CreateEdit{{EntityInfo.Name}}ViewModel.cs
+++ src/AbpHelper/Templates/Crud/Groups/UiRazor/src/{{ProjectInfo.FullName}}.Web/Pages/{{Bag.PagesFolder}}/{{EntityInfo.RelativeDirectory}}/{{EntityInfo.Name}}/ViewModels/CreateEdit{{EntityInfo.Name}}ViewModel.cs
@@ -9,31 +9,33 @@ namespace {{ ProjectInfo.FullName }}.Web.Pages.{{ pagesNamespace }}{{ EntityInfo
 {
     public class CreateEdit{{ EntityInfo.Name }}ViewModel
     {
+        {{~ typePrimitive = ["int","object","short","char","float","double","bool","string","sbyte","decimal","uint","long","ulong","ushort","dynamic","Boolean","Byte","Int16","UInt16","Int32","UInt32","Int64","UInt64","Single", "DateTime"] ~}}
         {{~ for prop in EntityInfo.Properties ~}}
         {{~ if prop | abp.is_ignore_property; continue; end ~}}
         {{~ if !Option.SkipLocalization ~}}
         [Display(Name = "{{ EntityInfo.Name + prop.Name}}")]
         {{~ end ~}}
-        {{~ if string.ends_with prop.Type ">" ~}}     
-        
+        {{~ if string.ends_with prop.Type ">" ~}}        
         public {{
         stNameRgex = prop.Name | regex.replace "s" "." "$"
         stStart = "<" | string.append stNameRgex
         stViewModels = stStart | string.append "ViewModels."
         stCreateEdit = stViewModels | string.append "CreateEdit"
         stNameRgexTwo = prop.Name | regex.replace "s" "ViewModel" "$"
-        stArrangeName = stViewModelsEx | string.append stNameRgexTwo
+        stArrangeName = stCreateEdit | string.append stNameRgexTwo
         stAppendAtEnd = stArrangeName | string.append ">"
         stRegexFind = prop.Name | regex.replace "s" ">" "$"
-
         stFind = "<" | string.append stRegexFind
-        
         prop.Type | string.replace stFind stAppendAtEnd
-
-        }} CreateEdit{{ prop.Name }}ViewModel { get; set; }
+        }} createEdit{{ prop.Name }}ViewModel { get; set; }
         {{~ else ~}}
-        public {{ prop.Type}} {{ prop.Name }} { get; set; }
-        {{~end ~}}
+        {{~ validateOfPrimite = typePrimitive | array.contains prop.Type ~}}
+        {{~ if !validateOfPrimite ~}}
+        public {{ prop.Name }}.ViewModels.CreateEdit{{ prop.Type }}ViewModel createEdit{{ prop.Name }}ViewModel { get; set; }
+        {{~ else ~}}
+        public {{ prop.Type }} {{ prop.Name }} { get; set; }
+        {{~ end ~}}
+        {{~ end ~}}
         {{~ if !for.last ~}}
 
         {{~ end ~}}
